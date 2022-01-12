c7faa1702188ccd855afe1d0a5af7cd86b800cb4
Abdullah-MotiWala <motiwalaabdullah100@gmail.com>
2022-01-12 14:02:24 +0500

    defactoring of function

--- admin.js
+++ admin.js
@@ -9,7 +9,11 @@ let qNo = 0;
 let singleArr = [];
 let multiArr = [];
 let dataArr = [];
-let quesObj = { singleArr, multiArr, dataArr };
+let quesObj = {
+  singleArr,
+  multiArr,
+  dataArr
+};
 let singleQueObj = {};
 
 quesBtn.addEventListener("click", () => {
@@ -26,92 +30,9 @@ const sendingToDb = () => {
 const addQue = () => {
   queDetailDiv = document.createElement("div");
   queDetailDiv.setAttribute("class", "questionDiv");
-  let radArr = document.querySelector("input[name='qType']:checked").value;
-
-  //creating save btn
-  let saveBtn = document.createElement("button");
-  let btnText = document.createTextNode("Save");
-  saveBtn.appendChild(btnText);
-
-  //creating question Bar
-
-  // data type question
-  if (radArr === "data") {
-    qType = "data";
-    qBar = document.createElement("input");
-    qBar.setAttribute("name", `data${qNo}`);
-    qBar.setAttribute("class", `dataqBar${qNo}`);
-
-    queDetailDiv.classList.add(`data${qNo}`);
-    queDetailDiv.classList.add(`data`);
-    qNo++;
-  } //single question
-  else if (radArr === "single") {
-    qBar = document.createElement("input");
-    qBar.setAttribute("name", `single${qNo}`);
-    qBar.setAttribute("class", `singleqBar${qNo}`);
-
-    queDetailDiv.classList.add(`single${qNo}`);
-    queDetailDiv.classList.add(`single`);
-
-    qType = "single";
-    let opt = ["true", "false"];
-    //creating radio btns for true & false (single questions)
-    for (let i = 0; i < opt.length; i++) {
-      let textNode = document.createTextNode(opt[i]);
-      let creLabel = document.createElement("label");
-      let createRad = document.createElement("input");
-      createRad.setAttribute("type", "radio");
-      createRad.setAttribute("name", `single${qNo}`);
-      createRad.setAttribute("value", opt[i]);
-      if (i == 0) {
-        createRad.setAttribute("checked", "checked");
-      }
-      creLabel.appendChild(textNode);
-      creLabel.appendChild(createRad);
-      //appending to question bar
-      queDetailDiv.appendChild(creLabel);
-    }
-    qNo++;
-  }
-  //multiple questions
-  else if (radArr === "multi") {
-    qBar = document.createElement("input");
-    qBar.setAttribute("name", `multi${qNo}`);
-    qBar.setAttribute("class", `multiqBar${qNo}`);
-
-    queDetailDiv.classList.add(`multi${qNo}`);
-    queDetailDiv.classList.add(`multi`);
-    qType = "multi";
-    //creating ul for options
-    let ulForOpt = document.createElement("ul");
-    ulForOpt.setAttribute("class", `ulForOpt${qNo}`);
-    let options = prompt("Enter the no. of options");
-    for (let i = 0; i < options; i++) {
-      // creating li options
-      let liForOpt = document.createElement("li");
-      liForOpt.setAttribute("class", `liOpt${qNo}`);
-      textForOpt = document.createElement("input");
-      textForOpt.setAttribute("name", "txtOpts");
-      let checkCorrect = document.createElement("input");
-      checkCorrect.setAttribute("type", "radio");
-      checkCorrect.setAttribute("name", `multi${qNo}`);
-      checkCorrect.setAttribute("value", i);
-
-      liForOpt.appendChild(textForOpt);
-      liForOpt.appendChild(checkCorrect);
-      // appending to ul
-      ulForOpt.appendChild(liForOpt);
-    }
-    //appending to question bar
-    queDetailDiv.appendChild(ulForOpt);
-    qNo++;
-  }
-  quesDiv.appendChild(queDetailDiv);
-  queDetailDiv.appendChild(qBar);
-  queDetailDiv.appendChild(saveBtn);
-  // sendingToDb();
-};
+  let quesType = document.querySelector("input[name='qType']:checked").value;
+  createQue(quesType)
+}
 
 //FIREBAE FUNC 1:authStateChange
 auth.onAuthStateChanged((user) => {
@@ -180,14 +101,16 @@ const logOut = () => {
 //DATABASE FUNC 1(): creating obj for db
 const createDBObj = () => {
   const questions = quesDiv.children;
+  console.log(questions)
   for (let i = 0; i < questions.length; i++) {
     console.log(questions[i]);
+
+
     //sending single question to db
     if (questions[i].classList.contains(`single${i}`)) {
       singleArr.push({
         q: document.querySelector(`.singleqBar${i}`).value,
-        isTrue:
-          document.querySelector(`input[name = single${i}]:checked`).value ==
+        isTrue: document.querySelector(`input[name = single${i}]:checked`).value ==
           "true"
       });
     }
@@ -201,6 +124,7 @@ const createDBObj = () => {
 
     //multiQuestion to Db
     else if (questions[i].classList.contains(`multi${i}`)) {
+      console.log(document.querySelector(`.ulForOpt${0}`).children)
       let optionsUl = document.querySelector(`.ulForOpt${i}`).children;
       let options = [];
       for (let i = 0; i < optionsUl.length; i++) {
@@ -229,3 +153,74 @@ const saveInDb = (obj) => {
       alert("Error writing document: ", error);
     });
 };
+
+
+//DOM FUNCTION 1: creating question 
+const createQue = (qType) => {
+  //creating Questions
+  qBar = document.createElement("input");
+  qBar.setAttribute("name", `${qType}${qNo}`);
+  qBar.setAttribute("class", `${qType}qBar${qNo}`);
+
+  queDetailDiv.classList.add(`${qType}${qNo}`);
+  queDetailDiv.classList.add(qType);
+ 
+
+  if (qType == 'single') {
+    creRad(['true', 'false']);
+  }
+  if (qType == 'multi') {
+    console.log(qType);
+    queDetailDiv.appendChild(creUl(qType));
+  }
+
+  quesDiv.appendChild(queDetailDiv);
+  queDetailDiv.appendChild(qBar);
+  qNo++;
+}
+
+
+//DOM FUNCTION 2: creating Radio for single Questions 
+const creRad = (opt) => {
+  for (let i = 0; i < opt.length; i++) {
+    let textNode = document.createTextNode(opt[i]);
+    let creLabel = document.createElement("label");
+    let createRad = document.createElement("input");
+    createRad.setAttribute("type", "radio");
+    createRad.setAttribute("name", `single${qNo}`);
+    createRad.setAttribute("value", opt[i]);
+    if (i == 0) {
+      createRad.setAttribute("checked", "checked");
+    }
+    creLabel.appendChild(textNode);
+    creLabel.appendChild(createRad);
+    //appending to question bar
+    queDetailDiv.appendChild(creLabel);
+  }
+}
+
+// DOM FUCNTION 3: creating ul for multi Questions
+const creUl = (type) => {
+  console.log(type)
+  //creating ul for options
+  let ulForOpt = document.createElement("ul");
+  ulForOpt.setAttribute("class", `ulForOpt${qNo}`);
+  let options = prompt("Enter the no. of options");
+  for (let i = 0; i < options; i++) {
+    // creating li options
+    let liForOpt = document.createElement("li");
+    liForOpt.setAttribute("class", `liOpt${qNo}`);
+    textForOpt = document.createElement("input");
+    textForOpt.setAttribute("name", "txtOpts");
+    let checkCorrect = document.createElement("input");
+    checkCorrect.setAttribute("type", "radio");
+    checkCorrect.setAttribute("name", `${type}${qNo}`);
+    checkCorrect.setAttribute("value", i);
+
+    liForOpt.appendChild(textForOpt);
+    liForOpt.appendChild(checkCorrect);
+    // appending to ul
+    ulForOpt.appendChild(liForOpt);
+  }
+  return ulForOpt
+}
